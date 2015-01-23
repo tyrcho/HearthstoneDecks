cb5a50206eb0bf94cc53ffd0bd61dab62cfc0d14
MICHEL DAVIOT <michel.daviot@atos.net>
2015-01-23 18:40:08 +0100

    scraps gosugamers.net

--- src/main/scala/info/daviot/demo/soup/Parser.scala
+++ src/main/scala/info/daviot/demo/soup/Parser.scala
@@ -9,12 +9,20 @@ import scala.concurrent.duration.DurationInt
 import grizzled.slf4j.Logging
 import org.jsoup.nodes.Document
 import dispatch._
+import org.jsoup.nodes.Element
 // check also : http://www.gosugamers.net/hearthstone/decks
+//See http://stackoverflow.com/questions/14253515/use-dispatch-0-9-5-behind-proxy for proxy
 object Parser extends App with Logging {
+  //  val decks = Await.result(
+  //    for {
+  //      urls <- HstdUrlExtract.extractFrom("http://www.hearthstonetopdecks.com/deck-category/deck-class/druid/")
+  //      decks <- Future.sequence(urls.map(HstdDeckExtract.extractFrom))
+  //    } yield decks, 1000.seconds)
+
   val decks = Await.result(
     for {
-      urls <- UrlExtract.extractFrom("http://www.hearthstonetopdecks.com/deck-category/deck-class/druid/")
-      decks <- Future.sequence(urls.map(DeckExtract.extractFrom))
+      urls <- GgUrlExtract.extractFrom("http://www.gosugamers.net/hearthstone/decks?name=&textMode=0&filter=Filter&class=16")
+      decks <- Future.sequence(urls.map(GgDeckExtract.extractFrom))
     } yield decks, 1000.seconds)
 
   println(decks)
@@ -36,7 +44,16 @@ object Parser extends App with Logging {
     for (data <- Http(svc OK as.String)) yield Jsoup.parse(data)
   }
 
-  object UrlExtract extends DataExtract[List[String]] {
+  object GgUrlExtract extends DataExtract[List[String]] {
+    def extractFrom(url: String): Future[List[String]] =
+      for {
+        doc <- readUrl(url)
+      } yield (for {
+        link <- doc.getElementsByAttributeValueStarting("href", "/hearthstone/decks/").toList
+      } yield "http://www.gosugamers.net" + link.attr("href")).distinct
+  }
+
+  object HstdUrlExtract extends DataExtract[List[String]] {
     def extractFrom(url: String): Future[List[String]] =
       for {
         doc <- readUrl(url)
@@ -45,12 +62,28 @@ object Parser extends App with Logging {
       } yield link.attr("href")).distinct
   }
 
-  object DeckExtract extends DataExtract[Deck] {
+  object HstdDeckExtract extends DataExtract[Deck] {
     def extractFrom(url: String): Future[Deck] = for {
       doc <- readUrl(url)
       content = doc.select("#neutral , #classes").select("li").map(_.text)
       cl = doc.select("span.deck-info").head.text.replaceAll("Class:", "").replaceAll("- Type.*", "").trim
     } yield Deck(doc.title, cl, Deck.parse(content), url)
+  }
+
+  object GgDeckExtract extends DataExtract[Deck] {
+    val classRe = """.*Class: (\w+).*""".r
+
+    def parseCards(content: Element) = for {
+      a <- content.select("a")
+      name = a.select("span.name").head.text
+      count = a.select("span.count").headOption.map(_.text.toInt).getOrElse(1)
+    } yield s"${count}x $name"
+
+    def extractFrom(url: String): Future[Deck] = for {
+      doc <- readUrl(url)
+      content = doc.select("div.deck").head
+      classRe(cl) = doc.select("div.deck-stat-summary").head.text
+    } yield Deck(doc.title, cl, Deck.parse(parseCards(content)), url)
   }
 
   case class Deck(name: String, hero: String, cards: List[Card], url: String) {
